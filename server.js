e0da7014ce21d7fa714d36207ed21bd6ff939da0
Dustin Honeck <13143062+dhoneck@users.noreply.github.com>
2023-02-13 21:45:51 -0600

    Add fsUtils.js file and add routes

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const { readFromFile, writeToFile, readAndAppend } = require('./helpers/fsUtils');
+
+
+const PORT = 3000;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
